36440fb0bbc5c0572860f1b00183968f050f7578
Jakub Nalewajk <38465906+jaqubowsky@users.noreply.github.com>
2023-03-27 01:55:26 +0200

    Revert "Add styled card container"
    This reverts commit 049ece8a5c837505671d6f0ba4e83c82a8fc0842.

--- src/components/Cards/CardContainer.styled.jsx
+++
@@ -1,5 +0,0 @@
-import StyledCardContainer from "../../styles/Cards/CardContainer";
-
-const CardContainer = ({ children }) => <StyledCardContainer>{children}</StyledCardContainer>;
-
-export default CardContainer;
