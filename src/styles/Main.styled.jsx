b8665b508f2c2a0e5ae25fae08daeaf5b63f99c0
Jakub Nalewajk <38465906+jaqubowsky@users.noreply.github.com>
2023-03-27 01:55:05 +0200

    Add styled main component

---
+++ src/styles/Main.styled.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+const StyledMain = styled.main`
+display: flex;
+justify-content: center;
+align-items: center;
+  margin: 0 auto;
+`
+
+export default StyledMain
