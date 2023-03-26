6d8b965dea7e38f9f20f61dca471a9a5607c25a9
Jakub Nalewajk <38465906+jaqubowsky@users.noreply.github.com>
2023-03-27 01:55:58 +0200

    Add card container component

---
+++ src/styles/Cards/CardContainer.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const StyledCardContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  max-width: 800px;
+
+  gap: 1em;
+  padding: 1em;
+`;
+
+export default StyledCardContainer;
