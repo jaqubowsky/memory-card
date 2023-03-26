9cc709cc87ecdb462e8d8e057f10de59a29b1cb2
Jakub Nalewajk <38465906+jaqubowsky@users.noreply.github.com>
2023-03-27 01:56:13 +0200

    Change loader's color

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,7 +1,7 @@
 import ClipLoader from "react-spinners/ClipLoader";
 
 function Loader() {
-  return <ClipLoader color="#ba17eb" loading size={100} />;
+  return <ClipLoader color="#913175" loading size={100} />;
 }
 
 export default Loader;
