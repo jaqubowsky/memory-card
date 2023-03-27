666d02e16b5578d9318106b874cce29af6534ed9
Jakub Nalewajk <38465906+jaqubowsky@users.noreply.github.com>
2023-03-27 16:45:49 +0200

    Add styled card container

---
+++ src/styles/Cards/CardContainer.styled.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const StyledCardContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  max-width: 800px;
+  height: 800px;
+
+  gap: 1em;
+  padding: 1em;
+`;
+
+export default StyledCardContainer;
