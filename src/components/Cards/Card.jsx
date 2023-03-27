01a6aaf251d3dbebfb2493fcb01fcdaf52ebe3d3
Jakub Nalewajk <38465906+jaqubowsky@users.noreply.github.com>
2023-03-27 16:46:50 +0200

    Add prop types and handle onClick

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,12 +1,20 @@
+import { PropTypes } from "prop-types";
 import StyledCard from "../../styles/Cards/Card.styled";
 
-function Card({ name, img }) {
+function Card({ name, img, handleClick, id }) {
   return (
-    <StyledCard>
+    <StyledCard onClick={handleClick} id={id}>
       <img src={img} alt={name} />
       <h2>{name}</h2>
     </StyledCard>
   );
 }
+
+Card.propTypes = {
+  name: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+  img: PropTypes.string.isRequired,
+  handleClick: PropTypes.func.isRequired,
+};
 
 export default Card;
