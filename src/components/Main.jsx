0fb19845a6ba803fd70e3ab39bf5cf689716fe15
Jakub Nalewajk <38465906+jaqubowsky@users.noreply.github.com>
2023-03-27 16:46:17 +0200

    Add picked pokemons to state and update score

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import usePokemonData from "../hooks/usePokemonData";
 import Loader from "./Loader/Loader";
 import { Card, CardContainer } from "./Cards";
@@ -5,7 +6,21 @@ import StyledMain from "../styles/Main.styled";
 import { randomiseArray } from "../utils/index";
 
 function Main() {
+  const [pickedPokemons, setPickedPokemons] = useState([]);
+  const [currentScore, setCurrentScore] = useState(0);
   const POKEMON_LIMIT = "12";
+
+  const addPickedPokemon = (pokemonId) => {
+    setPickedPokemons((prevPokemon) => {
+      if (prevPokemon.includes(pokemonId)) {
+        setCurrentScore(0);
+        return [];
+      }
+
+      return [...prevPokemon, pokemonId];
+    });
+    setCurrentScore((prevScore) => prevScore + 1);
+  };
 
   const { pokemonData, isLoading, error } = usePokemonData(
     `https://pokeapi.co/api/v2/pokemon/?limit=${POKEMON_LIMIT}`,
@@ -15,7 +30,13 @@ function Main() {
     const randomisedPokemonData = randomiseArray(pokemonData);
 
     const pokemonObject = randomisedPokemonData.map((pokemon) => (
-      <Card name={pokemon.name} img={pokemon.img} key={pokemon.id} />
+      <Card
+        handleClick={(e) => addPickedPokemon(e.currentTarget.id)}
+        name={pokemon.name}
+        img={pokemon.img}
+        key={pokemon.id}
+        id={pokemon.id}
+      />
     ));
 
     return (
